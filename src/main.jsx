682cae5d8f750c881c4145e9a143016fa91d16db
Adityakbr01 <aditykbr01@gmail.com>
2026-08-04 11:55:20 +0530

    Add confirm dialog and axios interceptor; update auth, brand list, and DC receipt received

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./lib/axiosInterceptor.js";
 import AppProvider from "./lib/ContextPanel.jsx";
 import App from "./App.jsx";
 import "./index.css";
